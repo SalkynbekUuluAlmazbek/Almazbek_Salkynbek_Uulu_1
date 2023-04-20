aa9faca7844142f97b39c6910e0da066afd7ea7d
SalkynbekUuluAlmazbek <salkynbekovalmaz@gmail.com>
2023-04-20 17:42:40 +0600

    Initial commit

---
+++ app/src/main/java/com/example/almazbek_salkynbek_uulu_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.almazbek_salkynbek_uulu_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
